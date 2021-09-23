aedfc9833737c29241e34c316826322b8fbefd40
adamz <58021562+ad4mny@users.noreply.github.com>
2021-09-24 06:01:08 +0800

    updated greeting message to greet nicknames on landing page

--- www/js/app/app.js
+++ www/js/app/app.js
@@ -1,14 +1,24 @@
-var web_links = "https://umpehungry.000webhostapp.com/";
-var user_token = JSON.parse(localStorage.getItem('users'));
-var orders_token = JSON.parse(localStorage.getItem('orders'));
+var web_links = "http://localhost/foodbank-management-system/";
+var ugf_user_token = JSON.parse(localStorage.getItem('ugf_users'));
 
 window.addEventListener('load', (event) => {
-    if (user_token != null) {
-        $('#profile_button').html(
-            '<a href="profile.html" class="btn position-relative">' +
-            '<i class="fas fa-user fa-fw fa-lg"></i>' +
+    if (ugf_user_token != null) {
+        $('#user_button').html(
+            '<a href="#" class="btn text-white" id="logout">' +
+            '<i class="fas fa-sign-out-alt fa-fw fa-lg"></i>' +
             '</a>'
         );
+
+        var nick = ugf_user_token.full_name.split(" ");
+        $('#greets').html(
+            '            <div class="col text-center">' +
+            '                <h1 class="text-capitalize text-white">Welcome, ' + nick[0] + '.</h1>' +
+            '                <p class="lead text-white">Take some meals or groceries to feed your tummy.</p><a' +
+            '                    href="foodbank.html"' +
+            '                    class="btn btn-outline-light rounded-0">View foodbank</a>' +
+            '            </div>'
+        );
+
     }
 })
 
@@ -130,5 +140,12 @@ $(document).on('click', '#scanPay', function () {
         disableAnimations: true, // iOS
         disableSuccessBeep: false // iOS and Android
     });
+
+});
+
+$(document).on('click', '#logout', function () {
+
+    localStorage.clear();
+    location.replace('index.html');
 
 });
